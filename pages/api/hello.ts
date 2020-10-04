725dc64ef27f88f55bed9d5544c70bbc1435d979
Jim Geurts <jim@biacreations.com>
2020-10-03 19:57:59 -0500

    Remove hello world api

--- pages/api/hello.ts
+++
@@ -1,6 +0,0 @@
-import type { NowRequest, NowResponse } from '@vercel/node';
-
-export default (_req: NowRequest, res: NowResponse): void => {
-  res.statusCode = 200;
-  res.json({ name: 'John Doe' });
-};
