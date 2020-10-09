e89132282919ac90d34fe10db754ed76020e13d1
Jim Geurts <jim@biacreations.com>
2020-10-09 17:25:36 -0500

    Add redirects for old urls

--- next.config.js
+++ next.config.js
@@ -3,4 +3,53 @@
 module.exports = {
   poweredByHeader: false,
   reactStrictMode: true,
+  async redirects() {
+    // const artistApi = new Artists();
+    // const artists = await artistApi.listAllArtists();
+
+    return [
+/*      ...artists.map((artist) => {
+        return {
+          source: `/artists/${artist.id}`,
+          destination: `/artists/${artist.id}/${slugify(artist.name)}`,
+          permanent: false,
+        };
+      }),*/
+      {
+        source: '/Artists/Detail/:slug*',
+        destination: '/artists/:slug*',
+        permanent: true,
+      },
+      {
+        source: '/Concerts/Detail/:slug*',
+        destination: '/concerts/:slug*',
+        permanent: true,
+      },
+      {
+        source: '/Compilations/Detail/:slug*',
+        destination: '/compilations/:slug*',
+        permanent: true,
+      },
+      {
+        source: '/Playlists/Detail/:slug*',
+        destination: '/playlists/:slug*',
+        permanent: true,
+      },
+      {
+        source: '/Tours/Detail/:slug*',
+        destination: '/tours/:slug*',
+        permanent: true,
+      },
+      {
+        source: '/Tours/RandomConcert/:slug*',
+        destination: '/tours/:slug*/random',
+        permanent: true,
+      },
+      {
+        source: '/Users/Detail/:slug*',
+        destination: '/users/:slug*',
+        permanent: true,
+      },
+    ];
+  }
 }
