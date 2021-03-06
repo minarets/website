f344f349f70ec2c7e6ae26ebb8347be085f051b9
Jim Geurts <jim@biacreations.com>
2021-03-06 09:41:47 -0600

    Improve 404 page layout

--- pages/404.tsx
+++ pages/404.tsx
@@ -14,11 +14,11 @@ export default function NotFound(): ReactElement {
         <title>{title} · Minarets</title>
       </Head>
 
-      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+      <div className="py-2 mb-4 border-bottom">
         <h1 className="h2">Page Not Found</h1>
-
-        <p>Tell me what it is you think you&apos;re missing and I will see what I can find</p>
       </div>
+
+      <p>Tell me what it is you think you&apos;re missing and I will see what I can find</p>
     </>
   );
 }
