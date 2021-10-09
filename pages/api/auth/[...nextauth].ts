8a6415183ded263242a4a26a9a1997aa5ddc9142
Jim Geurts <jim@biacreations.com>
2021-10-09 13:20:12 -0500

    Use email port from env variable if defined

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -12,7 +12,7 @@ const providers: AppProviders = [
   Providers.Email({
     server: {
       host: process.env.EMAIL_SERVER_HOST || '',
-      port: 587,
+      port: process.env.EMAIL_SERVER_PORT || 587,
       auth: {
         user: process.env.EMAIL_SERVER_USER || '',
         pass: process.env.EMAIL_SERVER_PASSWORD || '',
